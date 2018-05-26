0888ba84ba2f84962bab371217a88041f1abd63a
Marat Habibullin <maratx387@gmail.com>
2018-05-26 16:50:14 +0300

    Make grid size customizable and adjust its presentation to window sizes

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"golang.org/x/image/colornames"
 )
 
+const (
+	screenWidth  = 800
+	screenHeight = 600
+)
+
+const (
+	gridWidth  = 20
+	gridHeight = 8
+	cellSize   = screenWidth / gridWidth * 0.8
+)
+
 func run() {
 	cfg := pixelgl.WindowConfig{
-		Title:     "HoMM Go",
-		Bounds:    pixel.R(0, 0, 800, 600),
-		VSync:     true,
+		Title:  "HoMM Go",
+		Bounds: pixel.R(0, 0, screenWidth, screenHeight),
+		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
@@ -21,13 +32,18 @@ func run() {
 	backMat := pixel.IM
 	backMat = backMat.Scaled(pixel.ZV, 0.4)
 	backMat = backMat.Moved(win.Bounds().Center().Add(pixel.V(0, 40)))
-	grid := createGrid(pixel.V(50, 50), 14, 7, 50)
+
+	grid := createGrid(gridPos(), gridWidth, gridHeight, cellSize)
 	for !win.Closed() {
 		win.Clear(colornames.Green)
 		background.Draw(win, backMat)
 		drawGrid(win, grid)
 		win.Update()
 	}
+}
+
+func gridPos() pixel.Vec {
+	return pixel.V((screenWidth-gridWidth*cellSize)/2, (screenHeight-gridHeight*cellSize)/2 * 0.6)
 }
 
 func main() {
